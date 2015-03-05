edc29402484e2ae91d7baa2fa294f7060ffeebfb
David Pilato <david@pilato.fr>
2015-03-05 16:30:27 +0100

    Use directly Hibernate 4.3.8.Final instead of Spring
    Closes #7.
    (cherry picked from commit 6231764)
    (cherry picked from commit ddc7a4a)
    (cherry picked from commit 12c6988)
    (cherry picked from commit b9ecfcf)

--- src/main/java/fr/pilato/demo/legacysearch/dao/SearchDaoImpl.java
+++ src/main/java/fr/pilato/demo/legacysearch/dao/SearchDaoImpl.java
@@ -4,17 +4,11 @@ package fr.pilato.demo.legacysearch.dao;
 import fr.pilato.demo.legacysearch.domain.Person;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataAccessException;
-import org.springframework.orm.hibernate3.HibernateTemplate;
-import org.springframework.orm.hibernate3.SessionFactoryUtils;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 
@@ -24,31 +18,17 @@ import java.util.Collection;
  * 
  * @author David Pilato
  */
-@Repository
-@Transactional
 public class SearchDaoImpl implements SearchDao {
     final Logger logger = LoggerFactory.getLogger(SearchDaoImpl.class);
 
-    protected HibernateTemplate template = null;
-
-    /**
-     * Sets Hibernate session factory.
-     */
-    public void setSessionFactory(SessionFactory sessionFactory) {
-        template = new HibernateTemplate(sessionFactory);
-    }
-    
     /**
      * Find persons by any column (like full text).
      */
     @SuppressWarnings("unchecked")
-    public Collection<Person> findLikeGoogle(String query, Integer from, Integer size) throws DataAccessException {
-        Session session = SessionFactoryUtils.getSession(template.getSessionFactory(), true);
-
-        Criteria criteria = generateQuery(session, Person.class, query);
+    public Collection<Person> findLikeGoogle(String query, Integer from, Integer size) {
+        Criteria criteria = generateQuery(HibernateUtils.getSession(), Person.class, query);
         criteria.setFirstResult(from);
         criteria.setMaxResults(size);
-
         return criteria.list();
     }
 
@@ -57,8 +37,8 @@ public class SearchDaoImpl implements SearchDao {
      */
     @Override
     public long countLikeGoogle(String query) throws Exception {
-        Session session = SessionFactoryUtils.getSession(template.getSessionFactory(), true);
-        return generateQuery(session, Person.class, query).list().size();
+        int size = generateQuery(HibernateUtils.getSession(), Person.class, query).list().size();
+        return size;
     }
 
     /**
@@ -66,9 +46,7 @@ public class SearchDaoImpl implements SearchDao {
      */
     @SuppressWarnings("unchecked")
     public Collection<Person> findWithCriterias(Collection<Criterion> criterions, Integer from, Integer size) throws Exception {
-        Session session = SessionFactoryUtils.getSession(template.getSessionFactory(), true);
-
-        Criteria criteria = session.createCriteria(Person.class);
+        Criteria criteria = HibernateUtils.getSession().createCriteria(Person.class);
         criteria.createAlias("address", "address");
 
         for (Criterion crit : criterions) {
@@ -86,9 +64,7 @@ public class SearchDaoImpl implements SearchDao {
      */
     @SuppressWarnings("unchecked")
     public long countWithCriterias(Collection<Criterion> criterions) throws Exception {
-        Session session = SessionFactoryUtils.getSession(template.getSessionFactory(), true);
-
-        Criteria criteria = session.createCriteria(Person.class);
+        Criteria criteria = HibernateUtils.getSession().createCriteria(Person.class);
         criteria.createAlias("address", "address");
 
         for (Criterion crit : criterions) {
