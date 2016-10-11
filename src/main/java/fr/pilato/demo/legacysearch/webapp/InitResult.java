20fcd700afef1a8123d8910dc76898ab209bbe50
David Pilato <david@pilato.fr>
2016-10-11 20:53:55 +0200

    Add injection report
    Closes #40.
    
    (cherry picked from commit d8ab0ef)

--- src/main/java/fr/pilato/demo/legacysearch/webapp/InitResult.java
+++ src/main/java/fr/pilato/demo/legacysearch/webapp/InitResult.java
@@ -23,6 +23,7 @@ public class InitResult {
 
     private long took;
     private long rate;
+    private int current;
 
     public long getRate() {
         return rate;
@@ -32,8 +33,13 @@ public class InitResult {
         return took;
     }
 
-    public InitResult(long took, long rate) {
+    public int getCurrent() {
+        return current;
+    }
+
+    public InitResult(long took, long rate, int current) {
         this.took = took;
         this.rate = rate;
+        this.current = current;
     }
 }
