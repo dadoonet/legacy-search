a1551be59e053997912e63b0bbceaace82b347e8
David Pilato <david@pilato.fr>
2016-10-11 16:09:43 +0200

    Add progress bar
    Closes #39.
    
    (cherry picked from commit c005f8e)

--- src/main/webapp/init/init.component.js
+++ src/main/webapp/init/init.component.js
@@ -5,17 +5,43 @@ angular.
   module('init').
   component('init', {
     templateUrl: 'init/init.template.html',
-    controller: ['$http', 'config', function InitController($http, config) {
+    controller: ['$http', '$interval', 'config', function InitController($http, $interval, config) {
       var self = this;
       self.persons = "";
-      self.result = null;
+      self.status = "active";
+      self.progress = 0;
+
+      var stop;
+      self.startWatch = function() {
+        if ( angular.isDefined(stop) ) return;
+
+        stop = $interval(function() {
+          // Poll the status API
+          $http({method: 'GET', url: config.backend + '/api/1/person/_init_status' })
+              .success(function(data) {
+                self.progress = data;
+              });
+        }, 100);
+      };
+
+      self.stopWatch = function() {
+        if (angular.isDefined(stop)) {
+          $interval.cancel(stop);
+          stop = undefined;
+        }
+      };
 
       self.init = function() {
+        self.status = "active";
         self.result = false;
+        self.progress = 0;
+        self.goal = self.persons;
+        self.startWatch();
         $http({method: 'GET', url: config.backend + '/api/1/person/_init?size='+self.persons })
             .success(function(data, status, headers, config) {
-              self.result = true;
-              self.injected = self.persons;
+              self.progress = self.goal;
+              self.status = "progress-bar-success";
+              self.stopWatch();
         });
       }
     }]
