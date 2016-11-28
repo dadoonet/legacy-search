376fbd678991ab530a04e658551c427d0fa5ca02
David Pilato <david@pilato.fr>
2016-11-28 18:30:49 +0100

    Replace simpleQueryStringQuery with multiMatchQuery
    This will help to add fuzziness parameter and catch up with typos.
    
    Closes #43.
    
    (cherry picked from commit f2e6433)
    (cherry picked from commit d25b295)

--- src/main/java/fr/pilato/demo/legacysearch/service/PersonService.java
+++ src/main/java/fr/pilato/demo/legacysearch/service/PersonService.java
@@ -112,9 +112,10 @@ public class PersonService {
         if (!Strings.hasText(q)) {
             query = QueryBuilders.matchAllQuery();
         } else {
-            query = QueryBuilders.simpleQueryStringQuery(q)
-                    .field("fulltext")
-                    .field("name", 3.0f);
+            query = QueryBuilders
+                    .multiMatchQuery(q,
+                            "fulltext",
+                            "name^3.0");
         }
 
         SearchResponse response = elasticsearchDao.search(query, from, size);
