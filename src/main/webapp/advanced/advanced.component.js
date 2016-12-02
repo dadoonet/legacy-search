ac8a0b455edc8a70774a1422f59237d06218c7e1
David Pilato <david@pilato.fr>
2016-12-02 12:38:21 +0100

    Display message when backend is not available
    (cherry picked from commit f802101)
    
    (cherry picked from commit d73a699)
    
    (cherry picked from commit e6bb95a)
    
    (cherry picked from commit 8d6fcbb)
    
    (cherry picked from commit 5fd9fe2)
    
    (cherry picked from commit e3c6c68)

--- src/main/webapp/advanced/advanced.component.js
+++ src/main/webapp/advanced/advanced.component.js
@@ -14,11 +14,12 @@ angular.
       self.advanced_search = function() {
         $http({method: 'GET', url: '/api/1/person/_advanced_search?from=0&size=10&country='+self.country+'&city='+self.city+'&name='+ self.name })
             .success(function(data, status, headers, config) {
+              self.error = null;
               console.log(data.hits.total);
               self.result = data;
             })
             .error(function(data, status, headers, config) {
-              self.log = 'Error!';
+                self.error = "Backend not available";
             });
       };
 
