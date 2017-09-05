bf63529e5558056b2d28d516f11484e7c3dc1693
David Pilato <david@pilato.fr>
2017-09-05 11:40:33 +0200

    Update to elasticsearch 6.0.0-beta2 and to the HL Rest Client
    Closes #50.
    Closes #52.
    
    (cherry picked from commit a9832c5)

--- src/main/java/fr/pilato/demo/legacysearch/dao/ElasticsearchDao.java
+++ src/main/java/fr/pilato/demo/legacysearch/dao/ElasticsearchDao.java
@@ -23,49 +23,52 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.pilato.demo.legacysearch.domain.Person;
 import fr.pilato.elasticsearch.tools.ElasticsearchBeyonder;
+import org.apache.http.HttpHost;
 import org.elasticsearch.action.bulk.BulkProcessor;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.client.Client;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.node.Node;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.bucket.histogram.DateHistogramInterval;
 import org.elasticsearch.search.aggregations.bucket.histogram.ExtendedBounds;
-import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.threadpool.ThreadPool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import restx.factory.Component;
 
 import javax.inject.Inject;
-import java.net.InetSocketAddress;
+import java.io.IOException;
 
 @Component
 public class ElasticsearchDao {
     final Logger logger = LoggerFactory.getLogger(ElasticsearchDao.class);
 
     final private ObjectMapper mapper;
-    final private Client esClient;
+    final private RestHighLevelClient esClient;
     final private BulkProcessor bulkProcessor;
 
     @Inject
     public ElasticsearchDao(ObjectMapper mapper) {
-        this.esClient = new PreBuiltTransportClient(Settings.EMPTY)
-                .addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress("127.0.0.1", 9300)));
+        this.esClient = new RestHighLevelClient(RestClient.builder(HttpHost.create("http://127.0.0.1:9200")));
         // Automagically create index and mapping
         try {
-            ElasticsearchBeyonder.start(esClient);
+            ElasticsearchBeyonder.start(esClient.getLowLevelClient());
         } catch (Exception e) {
             logger.warn("can not create index and mappings", e);
         }
         this.mapper = mapper;
-        this.bulkProcessor = BulkProcessor.builder(esClient, new BulkProcessor.Listener() {
+        this.bulkProcessor = new BulkProcessor.Builder(esClient::bulkAsync, new BulkProcessor.Listener() {
             @Override
             public void beforeBulk(long executionId, BulkRequest request) {
                 logger.debug("going to execute bulk of {} requests", request.numberOfActions());
@@ -80,7 +83,7 @@ public class ElasticsearchDao {
             public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
                 logger.warn("error while executing bulk", failure);
             }
-        })
+        }, new ThreadPool(Settings.builder().put(Node.NODE_NAME_SETTING.getKey(), "high-level-client").build()))
                 .setBulkActions(10000)
                 .setFlushInterval(TimeValue.timeValueSeconds(5))
                 .build();
@@ -95,33 +98,31 @@ public class ElasticsearchDao {
         bulkProcessor.add(new DeleteRequest("person", "person", id));
     }
 
-    public SearchResponse search(QueryBuilder query, Integer from, Integer size) {
+    public SearchResponse search(QueryBuilder query, Integer from, Integer size) throws IOException {
         logger.debug("elasticsearch query: {}", query.toString());
-        SearchResponse response = esClient.prepareSearch("person")
-                .setTypes("person")
-                .setQuery(query)
-                .addAggregation(
-                        AggregationBuilders.terms("by_country").field("address.country.aggs")
-                                .subAggregation(AggregationBuilders.dateHistogram("by_year")
+        SearchResponse response = esClient.search(new SearchRequest("person")
+                .source(new SearchSourceBuilder()
+                        .query(query)
+                        .aggregation(
+                                AggregationBuilders.terms("by_country").field("address.country.aggs")
+                                        .subAggregation(AggregationBuilders.dateHistogram("by_year")
                                                 .field("dateOfBirth")
-                                                .minDocCount(0)
                                                 .dateHistogramInterval(DateHistogramInterval.days(3652))
                                                 .extendedBounds(new ExtendedBounds(1940L, 2009L))
                                                 .format("YYYY")
                                                 .subAggregation(AggregationBuilders.avg("avg_children").field("children"))
-                                )
-                )
-                .addAggregation(
-                        AggregationBuilders.dateHistogram("by_year")
-                                .field("dateOfBirth")
-                                .minDocCount(0)
-                                .dateHistogramInterval(DateHistogramInterval.YEAR)
-                                .extendedBounds(new ExtendedBounds(1940L, 2009L))
-                                .format("YYYY")
-                )
-                .setFrom(from)
-                .setSize(size)
-                .execute().actionGet();
+                                        )
+                        )
+                        .aggregation(
+                                AggregationBuilders.dateHistogram("by_year")
+                                        .field("dateOfBirth")
+                                        .dateHistogramInterval(DateHistogramInterval.YEAR)
+                                        .extendedBounds(new ExtendedBounds(1940L, 2009L))
+                                        .format("YYYY")
+                        )
+                        .from(from)
+                        .size(size)
+                ));
 
         logger.debug("elasticsearch response: {} hits", response.getHits().getTotalHits());
         logger.trace("elasticsearch response: {} hits", response.toString());
