fe0e71efcfe12f9567fa2d16f074c871b5a6c21e
David Pilato <david@pilato.fr>
2018-09-13 19:49:16 +0200

    Migration to Spring Boot
    This commit migrates the whole demo to Spring Boot instead of RestX.

--- src/main/java/fr/pilato/demo/legacysearch/domain/Marketing.java
+++ src/main/java/fr/pilato/demo/legacysearch/domain/Marketing.java
@@ -1,7 +1,5 @@
 package fr.pilato.demo.legacysearch.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,7 +11,10 @@ import javax.persistence.Id;
  */
 @Entity
 public class Marketing {
-    private Integer id = null;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
 
     private Integer cars;
     private Integer shoes;
@@ -25,19 +26,10 @@ public class Marketing {
     private Integer hifi;
     private Integer food;
 
-    /**
-     * Gets id (primary key).
-     */
-    @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
-    @JsonIgnore
     public Integer getId() {
         return id;
     }
 
-    /**
-     * Sets id (primary key).
-     */
     public void setId(Integer id) {
         this.id = id;
     }
@@ -113,54 +105,4 @@ public class Marketing {
     public void setFood(Integer food) {
         this.food = food;
     }
-
-    /**
-     * Returns a string representation of the object.
-     */
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-
-        sb.append(this.getClass().getName() + "-");
-        sb.append("  cars=" + cars);
-        sb.append("  shoes=" + shoes);
-        sb.append("  toys=" + toys);
-        sb.append("  fashion=" + fashion);
-        sb.append("  music=" + music);
-        sb.append("  garden=" + garden);
-        sb.append("  electronic=" + electronic);
-        sb.append("  hifi=" + hifi);
-        sb.append("  food=" + food);
-
-        return sb.toString();
-    }
-
-    /**
-     * Indicates whether some other object is equal to this one.
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Marketing other = (Marketing) obj;
-
-        if (cars != other.cars) return false;
-        if (shoes != other.shoes) return false;
-        if (toys != other.toys) return false;
-        if (fashion != other.fashion) return false;
-        if (music != other.music) return false;
-        if (garden != other.garden) return false;
-        if (electronic != other.electronic) return false;
-        if (hifi != other.hifi) return false;
-        if (food != other.food) return false;
-
-        return true;
-    }
-
-
-
 }
