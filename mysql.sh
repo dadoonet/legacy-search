a0068de02d8a21570654353e56d3c143048e2c14
David Pilato <david@pilato.fr>
2019-06-17 15:57:28 +0200

    Use Docker to run MySQL
    This will simplify running our demo app for any user.

---
+++ mysql.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+source .env
+
+docker stop legacy-mysql
+docker rm legacy-mysql
+docker run --name legacy-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=person -d mysql:$MYSQL_VERSION
+docker logs -f legacy-mysql
+
