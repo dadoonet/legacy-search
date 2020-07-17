bc9dbdeab93039807accfa25d28cb21347627cdb
David Pilato <david@pilato.fr>
2020-07-17 12:06:16 +0200

    Always pull the latest mysql version

--- mysql.sh
+++ mysql.sh
@@ -2,6 +2,7 @@
 
 source .env
 
+docker pull mysql:$MYSQL_VERSION
 docker stop legacy-mysql
 docker rm legacy-mysql
 docker run --name legacy-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=person -d mysql:$MYSQL_VERSION
