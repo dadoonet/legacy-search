3c20f94602174e02b940b49d007ba461d74b6298
David Pilato <david@pilato.fr>
2023-03-22 18:10:02 +0100

    Update libs
    * Elasticsearch 8.6.2
    * Spring boot 3.0.4
    * Jackson 2.14.2
    
    Closes #275.
    Closes #273.
    Closes #272.
    Closes #259.

--- src/main/java/fr/pilato/demo/legacysearch/domain/Person.java
+++ src/main/java/fr/pilato/demo/legacysearch/domain/Person.java
@@ -19,12 +19,12 @@
 
 package fr.pilato.demo.legacysearch.domain;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import java.time.LocalDate;
 
 @Entity
