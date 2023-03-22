ece36f5534d61be84d7b12db233c8da768c809c0
David Pilato <david@pilato.fr>
2023-03-22 18:10:02 +0100

    Update libs
    * Elasticsearch 8.6.2
    * Spring boot 3.0.4
    * Jackson 2.14.2
    
    Closes #275.
    Closes #273.
    Closes #272.
    Closes #259.

--- src/main/java/fr/pilato/demo/legacysearch/domain/Address.java
+++ src/main/java/fr/pilato/demo/legacysearch/domain/Address.java
@@ -19,11 +19,11 @@
 
 package fr.pilato.demo.legacysearch.domain;
 
-import javax.persistence.Embedded;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Address {
