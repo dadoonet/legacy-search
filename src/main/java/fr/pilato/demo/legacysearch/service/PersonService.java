73a8050869d99483fe6d2ea31889639870f27cdd
David Pilato <david@pilato.fr>
2024-06-11 12:48:04 +0200

    Make AtomicInteger final

--- src/main/java/fr/pilato/demo/legacysearch/service/PersonService.java
+++ src/main/java/fr/pilato/demo/legacysearch/service/PersonService.java
@@ -138,7 +138,7 @@ public class PersonService {
         return elasticsearchDao.search(query, from, size);
     }
 
-    private AtomicInteger currentItem = new AtomicInteger();
+    private final AtomicInteger currentItem = new AtomicInteger();
     private long start = 0;
 
     public InitResult init(Integer size) throws IOException {
