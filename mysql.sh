327b8728d696f5422809348a2e9e248b6c001c1f
David Pilato <david@pilato.fr>
2024-06-27 21:29:19 +0200

    Automatically stop the database
    When done with demo, we can stop it.

--- mysql.sh
+++ mysql.sh
@@ -8,3 +8,6 @@ docker rm legacy-mysql
 docker run --name legacy-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=person -d mysql:$MYSQL_VERSION --mysql-native-password=ON
 docker logs -f legacy-mysql
 
+# Stop the database when done
+docker stop legacy-mysql
+docker rm legacy-mysql
