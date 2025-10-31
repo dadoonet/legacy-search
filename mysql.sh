f233796cf8b277882eb2a001695a35a0b0d54ebf
David Pilato <david@pilato.fr>
2025-10-31 12:15:20 +0100

    Update to Elasticsearch 9.2 and start-local
    The client is much simpler to use.
    We remove our own made docker compose and use start local instead.

--- mysql.sh
+++ mysql.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-source .env
-
-docker pull mysql:$MYSQL_VERSION
+docker pull mysql:latest
 docker stop legacy-mysql
 docker rm legacy-mysql
-docker run --name legacy-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=person -d mysql:$MYSQL_VERSION
+docker run --name legacy-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=person -d mysql:latest
 docker logs -f legacy-mysql
 
 # Stop the database when done
